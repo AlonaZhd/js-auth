a7ecd5b8a001d28e37e8216e43731f5ef93ad682
AlonaZhd <alona.zdakaeva@gmail.com>
2023-09-10 22:35:06 +0200

    auth 2.0

--- src/container/signup/index.js
+++ src/container/signup/index.js
@@ -1,18 +1,107 @@
-class SingupForm {
-  static value = {}
+import {
+  Form,
+  REG_EXP_EMAIL,
+  REG_EXP_PASSWORD,
+} from '../../script/form'
 
-  static validate = (name, value) => {
-    return true
+class SingupForm extends Form {
+  FIELD_NAME = {
+    EMAIL: 'email',
+    PASSWORD: 'password',
+    PASSWORD_AGAIN: 'passwordAgain',
+    ROLE: 'role',
+    IS_CONFIRM: 'isConfirm',
   }
 
-  static submit = () => {
-    console.log(this.value)
+  FIELD_ERROR = {
+    IS_EMPTY: 'Введіть значення в поле',
+    IS_BIG: 'Дуже довге значення, прибуріть зайве',
+    EMAIL: 'Введіть корректне значення e-mail адреси',
+    PASSWORD:
+      'Пароль повинен складатись мінімум з 8 символів, включаючи хоча б одну цифру, велику та малу літеру',
+    PASSWORD_AGAIN: 'Паролі не співпадають',
+    NOT_CONFIRM: 'Ви не погоджуєтесь з правилами',
+    ROLE: 'Ви не обрали роль',
   }
 
-  static change = (name, value) => {
-    console.log(name, value)
-    if (this.validate(name, value)) this.value[name] = value
+  validate = (name, value) => {
+    if (String(value).length < 1) {
+      return this.FIELD_ERROR.IS_EMPTY
+    }
+
+    if (String(value).length > 20) {
+      return this.FIELD_ERROR.IS_BIG
+    }
+
+    if (name === this.FIELD_NAME.EMAIL) {
+      if (!REG_EXP_EMAIL.test(String(value)))
+        return this.FIELD_ERROR.EMAIL
+    }
+
+    if (name === this.FIELD_NAME.PASSWORD) {
+      if (!REG_EXP_PASSWORD.test(String(value)))
+        return this.FIELD_ERROR.PASSWORD
+    }
+
+    if (name === this.FIELD_NAME.PASSWORD_AGAIN) {
+      if (
+        String(value) !==
+        this.value[this.FIELD_NAME.PASSWORD]
+      ) {
+        return this.FIELD_ERROR.PASSWORD_AGAIN
+      }
+    }
+
+    if (name === this.FIELD_NAME.ROLE) {
+      if (isNaN(value)) return this.FIELD_ERROR.ROLE
+    }
+
+    if (name === this.FIELD_NAME.IS_CONFIRM) {
+      if (Boolean(value) !== true)
+        return this.FIELD_ERROR.NOT_CONFIRM
+    }
+  }
+
+  submit = async () => {
+    if (this.disabled === true) {
+      this.validateAll()
+    } else {
+      console.log(this.value)
+
+      this.setAlert('progress', 'Завантаження...')
+
+      try {
+        const res = await fetch('/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: this.convertData(),
+        })
+
+        const data = await res.json()
+
+        if (res.ok) {
+          this.setAlert('success', data.message)
+        } else {
+          this.setAlert('error', data.message)
+        }
+      } catch (error) {
+        this.setAlert('error', error.message)
+      }
+    }
+  }
+
+  convertData = () => {
+    return JSON.stringify({
+      [this.FIELD_NAME.EMAIL]:
+        this.value[this.FIELD_NAME.EMAIL],
+      [this.FIELD_NAME.PASSWORD]:
+        this.value[this.FIELD_NAME.PASSWORD],
+      [this.FIELD_NAME.ROLE]:
+        this.value[this.FIELD_NAME.ROLE],
+    })
   }
 }
 
-window.singupForm = SingupForm
+window.singupForm = new SingupForm()
